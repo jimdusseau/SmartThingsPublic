04a960e5a99fea037aef754dc0c8cc8bd4a60c9b
Jim Dusseau <jimdusseau@gmail.com>
2015-08-04 18:04:24 -0700

    Added Porch Light and Smart Fan apps

---
+++ smartapps/superuser/porch-light.src/porch-light.groovy
@@ -0,0 +1,92 @@
+// Automatically generated. Make future change here.
+definition(
+    name: "Porch Light",
+    namespace: "",
+    author: "Jim Dusseau",
+    description: "Turn on a light when you arrive home and it's after dark",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png")
+
+preferences {
+	section("When any of these people arrive") {
+        input "people", "capability.presenceSensor", multiple: true
+	}
+    
+    section("Turn on these lights") {
+		input "switches", "capability.switch", multiple: true
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	people.each {
+		subscribe(it.presence)
+	}
+}
+
+//---------
+
+def presence(evt)
+{
+	log.debug "evt.name: $evt.value"
+	def threshold = falseAlarmThreshold != null ? (falseAlarmThreshold * 60 * 1000) as Long : 10 * 60 * 1000L
+
+	if (location.mode != newMode) {
+
+		def t0 = new Date(now() - threshold)
+		if (evt.value == "present") {
+
+			def person = getPerson(evt)
+			def recentNotPresent = person.statesSince("presence", t0).find{it.value == "not present"}
+			if (recentNotPresent) {
+				log.debug "skipping notification of arrival of ${person.displayName} because last departure was only ${now() - recentNotPresent.date.time} msec ago"
+			}
+			else {
+				def message = "${person.displayName} arrived at home."
+				log.info message
+                turnOnLight()
+
+			}
+		}
+	}
+	else {
+		log.debug "mode is the same, not evaluating"
+	}
+}
+
+private getPerson(evt) {
+	people.find{evt.deviceId == it.id}
+}
+
+private turnOnLight() {
+    def sunRiseAndSunset = getSunriseAndSunset(sunsetOffset: -30)
+    def timenow = now()
+    if (timenow > sunRiseAndSunset.sunset.time  || timenow < sunRiseAndSunset.sunrise.time) {
+    	log.debug "$timenow is not between sunrise: $sunRiseAndSunset.sunrise.time and sunset: $sunRiseAndSunset.sunset.time . Turning on lights!"
+		def message = "Turning on porch light because someone's home and it's dark"
+        log.info message
+        //sendPush(message)
+        
+        for (aSwitch in switches) {
+        	log.debug "Turning on ${aSwitch.displayName}"
+        	aSwitch.on()
+        }
+    }
+    else {
+    	log.debug "$timenow is between sunrise: $sunRiseAndSunset.sunrise.time and sunset: $sunRiseAndSunset.sunset.time - Ignorning"
+    }
+}
---
+++ smartapps/superuser/smart-fan.src/smart-fan.groovy
@@ -0,0 +1,77 @@
+// Automatically generated. Make future change here.
+definition(
+    name: "Smart Fan",
+    namespace: "",
+    author: "Jim Dusseau",
+    description: "Turns on a fan when it's colder outside and the inside temp is higher than set.",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png")
+
+preferences {
+  section("Choose a temperature sensor... ") {
+    input "sensor", "capability.temperatureMeasurement", title: "Sensor"
+  }
+
+  section("Choose your fans...") {
+    input "switches", "capability.switch", multiple: true
+  }
+
+  section("What is your zip code?") {
+    input "zipcode", "text", title: "Zipcode?"
+  }
+
+  section("Turn fan off when it's colder than ") {
+    input "lowTemp", "number", title: "Degrees F"
+  }
+}
+
+def installed() {
+  initialize()
+  state.fanState = -1
+}
+
+def updated() {
+  unschedule()
+  initialize()
+}
+
+def initialize() {
+  scheduleCheck() 
+  runEvery5Minutes(scheduleCheck)
+}
+
+def scheduleCheck() {
+  def data = getWeatherFeature( "conditions", zipcode )
+  log.debug "Outside temp = $data.current_observation.temp_f  and internal temp = $sensor.currentTemperature"
+
+  def internalTemp = sensor.currentTemperature
+
+  def warmerOutside = data.current_observation.temp_f > internalTemp
+  log.debug "warmer outside: $warmerOutside Internal temp: $internalTemp low temp: $lowTemp"
+
+  if(!warmerOutside && internalTemp > lowTemp) {
+  	log.debug "Fans should be on"
+    if(state.fanState != 1) {
+        log.debug "Turning fans on"
+    	switches.on()
+        state.fanState = 1;
+    }
+    else {
+    	log.debug "Fans are already on"
+    }
+  }
+  else {
+	log.debug "Fans should be off"
+  	if(state.fanState != 0) {
+		log.debug "Turning fans off"
+    	switches.off()
+        state.fanState = 0
+    }
+    else {
+        log.debug "Fans are already off"
+    }
+    
+  }
+}
